a0a0c898e9ce92200baa397effdc9772f144c472
Cherise Foster <cherise.foster@outlook.com>
2023-06-29 19:40:59 -0600

    passed tests

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
+  def __init__(self, root=None):
     self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  def get_element_by_id(self, target_id):
+    return self._dfs(self.root, target_id)
+
+  def _dfs(self, node, target_id):
+    if node and node.get('id') == target_id:
+      return node
+
+    for child in node.get('children', []):
+      result = self._dfs(child, target_id)
+      if result:
+        return result
+
+    return None
